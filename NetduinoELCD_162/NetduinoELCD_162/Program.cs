c3354d59e9e389d3f5e0b2dc99a835bb3c503bed
Philippe <philippemariano@hotmail.fr>
2014-06-17 08:52:13 +0200

    Documentation

--- NetduinoELCD_162/NetduinoELCD_162/Program.cs
+++ NetduinoELCD_162/NetduinoELCD_162/Program.cs
@@ -12,12 +12,11 @@ namespace NetduinoELCD_162
     public class Program
     {
         public static void Main()
-        {  // Relier l'afficheur au connecteur O5 de la carte Tinkerkit
+        {  // Pour accéder au COM2, relier l'afficheur au connecteur KK O5 de la carte Tinkerkit
           int counter = 0;
           // Création d'un objet Afficheur LCD série (par défaut: COM2, 19200, None, 8, 1)
-
           SerialELCD162 ELCD162 = new SerialELCD162();
-          
+          // Initialisation du port série
           ELCD162.Init(); ELCD162.ClearScreen(); ELCD162.CursorOff();
          
           while (true)
