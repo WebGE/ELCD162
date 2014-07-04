642e9477c39fc7e4ae39b6164ad82a3db9d2da3e
WebGE <philippemariano@hotmail.fr>
2014-07-04 16:25:13 +0200

    Révision mineure

--- NetduinoELCD_162/NetduinoELCD_162/Program.cs
+++ NetduinoELCD_162/NetduinoELCD_162/Program.cs
@@ -16,7 +16,7 @@ namespace NetduinoELCD_162
           int counter = 0;
           // Création d'un objet Afficheur LCD série (par défaut: COM2, 19200, None, 8, 1)
           SerialELCD162 ELCD162 = new SerialELCD162();
-          // Initialisation du port série
+          // Initialisation du port série et de l'afficheur
           ELCD162.Init(); ELCD162.ClearScreen(); ELCD162.CursorOff();
          
           while (true)
