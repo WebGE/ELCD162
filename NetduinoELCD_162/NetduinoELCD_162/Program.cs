c470bf208addfb73a10caa2c50004613eaaf5be2
WebGE <philippemariano@hotmail.fr>
2014-12-26 19:55:08 +0100

    MàJ

--- NetduinoELCD_162/NetduinoELCD_162/Program.cs
+++ NetduinoELCD_162/NetduinoELCD_162/Program.cs
@@ -1,35 +1,34 @@
-﻿using System;
-using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Hardware;
-using SecretLabs.NETMF.Hardware;
-using SecretLabs.NETMF.Hardware.Netduino;
-
+﻿using System.Threading;
 using Serial;
 
-namespace NetduinoELCD_162
+namespace TestNetduinoELCD_162
 {
     public class Program
-    { // Page web http://webge.github.io/ELCD162/
+    {
         public static void Main()
-        {  // Pour accéder au COM2, relier l'afficheur au connecteur KK O5 de la carte Tinkerkit
-          int counter = 0;
-          // Création d'un objet Afficheur LCD série (par défaut: COM2, 19200, None, 8, 1)
-          SerialELCD162 ELCD162 = new SerialELCD162();
-          // Initialisation du port série et de l'afficheur
-          ELCD162.Init(); ELCD162.ClearScreen(); ELCD162.CursorOff();
-         
-          while (true)
-          {             
-              // Création d'une chaine de caractères
-              string counter_string = "Compte:" + counter.ToString() ;             
-              // Envoie des octets au port série
-              ELCD162.PutString(counter_string);                    
-              // Incrémentation du compteur
-              counter++;
-              // Attente de 1s entre deux envois
-              Thread.Sleep(1000); ELCD162.ClearScreen();
-          }
+        {// Documentation de la classe SerialELCD162 http://webge.github.io/ELCD162/
+            // Pour accéder au COM2, relier l'afficheur au connecteur O5 de la carte Tinkerkit
+            
+            // Valeur à afficher
+            var counter = 0;
+
+            // Création d'un objet afficheur série ELCD-162 (par défaut: COM2, 19200, None, 8, 1)
+            SerialELCD162 ELCD162 = new SerialELCD162();
+
+            // Initialisation du port série et de l’afficheur
+            ELCD162.Init(); ELCD162.ClearScreen(); ELCD162.CursorOff();
+
+            while (true)
+            {
+                // Création d'une chaine de caractères
+                string counter_string = "Compte:" + counter.ToString();
+                // Envoie des octets au port série de l'afficheur
+                ELCD162.PutString(counter_string);
+                // Incrémentation du compteur
+                counter++;
+                // Attente de 1s entre deux envois
+                Thread.Sleep(1000); ELCD162.ClearScreen();
+            }
         }
     }
 }
