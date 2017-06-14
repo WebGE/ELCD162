5173aca29257b25090937165a0a134b8250c39c4
Mariano Philippe <philippemariano@hotmail.fr>
2017-06-14 15:39:33 +0200

    Update v1.4.3.1

---
+++ ELCD162/Netduino/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using testMicroToolsKit.Hardware.Display;
+
+namespace Netduino
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Documentation on http://webge.github.io/ELCD162/
+            var counter = 0;
+
+            // ELCD-162 object (default : COM2, 19200, None, 8, 1)
+            ELCD162 display = new ELCD162();
+
+            // Init display serial port
+            display.Init(); display.ClearScreen(); display.CursorOff();
+
+            while (true)
+            {
+                var counter_string = "Compte:" + counter.ToString();
+                display.PutString(counter_string);
+                counter++;
+                Thread.Sleep(1000); display.ClearScreen();
+            }
+        }
+
+    }
+}
